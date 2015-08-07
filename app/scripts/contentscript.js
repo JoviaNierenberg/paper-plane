b4a4ac78ab8ff78df3c9fc221e469705011b9735
Jovia Nierenberg <nierenberg.jovia@gmail.com>
2015-08-06 20:30:30 -0400

    can display hello on page, working on displaying our overlay

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,29 @@
 'use strict';
 
+window.addEventListener("load", function(){
+	var app = angular.module("PaperPlane", []);
+
+	var html = document.querySelector('html');
+	html.setAttribute('ng-app', '');
+	html.setAttribute('ng-csp', '');
+
+	var overlay = document.getElementById("body");
+	overlay.setAttribute('ng-controller', 'MainCtrl');
+	app.controller('MainCtrl', function($scope) {});
+
+	var overlayDirective = document.createElement('div');
+	overlayDirective.setAttribute('overlay-directive', '');
+	overlay.appendChild(overlayDirective);
+
+	app.directive("overlayDirective", [ "$sce", function($sce){
+		return {
+			restrict: 'EA',
+			replace: true,
+			template: "<div>Hello</div>"//$sce.trustAsResourceUrl(chrome.extension.getURL('.././templates/overlay.html'))
+		}
+	}]);
+
+	angular.bootstrap(html, ['PaperPlane'], []);
+});
+
 console.log('\'Allo \'Allo! Content script');
